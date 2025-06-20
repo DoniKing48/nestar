d3b064a3144aecb2c0498f6231be24d823a714a4
DoniKing48 <overnelord@gmail.com>
2025-06-20 15:14:46 +0500

    fix: modification of property module

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -7,9 +7,7 @@ import { ApolloDriver } from '@nestjs/apollo';
 import { AppResolver } from './app.resolver';
 import { ComponentsModule } from './components/components.module';
 import { DatabaseModule } from './database/database.module';
-import { error } from 'console';
 import { T } from './libs/types/common';
-import { ViewService } from './components/view/view.service';
 
 @Module({
   imports: [
