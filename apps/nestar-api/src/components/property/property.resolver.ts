b17327be2f6f7e605e283f71fab18ff8538df70e
DoniKing48 <overnelord@gmail.com>
2025-06-23 11:24:58 +0500

    feat: develop getProperties GraphQL api

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -1,6 +1,6 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { PropertyService } from './property.service';
-import { PropertyInput } from '../../libs/dto/property/property.input';
+import { PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { MemberType } from '../../libs/enums/member.enum';
 import { UseGuards } from '@nestjs/common';
@@ -9,7 +9,7 @@ import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { ObjectId } from 'mongoose';
 import { WithoutGuard } from '../auth/guards/without.guard';
 import { shapeIntoMongoObjectId } from '../../libs/config';
-import { Property } from '../../libs/dto/property/property';
+import { Properties, Property } from '../../libs/dto/property/property';
 import { PropertyUpdate } from '../../libs/dto/property/property.update';
 
 @Resolver()
@@ -50,5 +50,15 @@ export class PropertyResolver {
     console.log('Mutation: updateProperty');
     input._id = shapeIntoMongoObjectId(input._id);
     return await this.propertyService.updateProperty(memberId, input);
+  }
+
+  @UseGuards(WithoutGuard)
+  @Query((returns) => Properties)
+  public async getProperties(
+    @Args('input') input: PropertiesInquiry,
+    @AuthMember('_id') memberId: ObjectId,
+  ): Promise<Properties> {
+    console.log('Query: getProperties');
+    return await this.propertyService.getProperties(memberId, input);
   }
 }
