cc6e49c5d524b65e99e2edd00faf3c380725b3f8
DoniKing48 <overnelord@gmail.com>
2025-06-27 16:20:17 +0500

    fix: fixed problems

--- apps/nestar-api/src/components/board-article/board-article.resolver.ts
+++ apps/nestar-api/src/components/board-article/board-article.resolver.ts
@@ -74,11 +74,11 @@ export class BoardArticleResolver {
 
   @Roles(MemberType.ADMIN)
   @Mutation((returns) => BoardArticle)
-  public async updateBoardArticlesByAdmin(
+  public async updateBoardArticleByAdmin(
     @Args('input') input: BoardArticleUpdate,
     @AuthMember('_id') memberId: ObjectId,
   ): Promise<BoardArticle> {
-    console.log('Mutation: updateBoardArticlesByAdmin');
+    console.log('Mutation: updateBoardArticleByAdmin');
     input._id = shapeIntoMongoObjectId(input._id);
     return await this.boardArticleService.updateBoardArticleByAdmin(input);
   }
@@ -86,12 +86,12 @@ export class BoardArticleResolver {
   @Roles(MemberType.ADMIN)
   @UseGuards(RolesGuard)
   @Mutation((returns) => BoardArticle)
-  public async removeBoardArticlesByAdmin(
+  public async removeBoardArticleByAdmin(
     @Args('articleId') input: string,
     @AuthMember('_id') memberId: ObjectId,
   ): Promise<BoardArticle> {
-    console.log('Mutation: removeBoardArticlesByAdmin');
+    console.log('Mutation: removeBoardArticleByAdmin');
     const articleId = shapeIntoMongoObjectId(input);
-    return await this.boardArticleService.removeBoardArticlesByAdmin(articleId);
+    return await this.boardArticleService.removeBoardArticleByAdmin(articleId);
   }  
 }
--- apps/nestar-api/src/components/board-article/board-article.service.ts
+++ apps/nestar-api/src/components/board-article/board-article.service.ts
@@ -170,7 +170,7 @@ export class BoardArticleService {
     return result;
   }
 
-  public async removeBoardArticlesByAdmin(articleId: ObjectId): Promise<BoardArticle> {
+  public async removeBoardArticleByAdmin(articleId: ObjectId): Promise<BoardArticle> {
     const search: T = { _id: articleId, articleStatus: BoardArticleStatus.DELETE};
     const result = await this.boardArticleModel.findOneAndDelete(search).exec();
     if (!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
