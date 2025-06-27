624ec4e23fbfe7913339b96a838ab556a24601a5
DoniKing48 <overnelord@gmail.com>
2025-06-27 14:48:08 +0500

    feat: develop createComment, updateComment and getComments GrapghQL APIs

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -154,7 +154,7 @@ export class MemberService {
     console.log('doneyykjsHAQGS')
     const { _id, targetKey, modifier} = input;
     return await this.memberModel
-      .findOneAndUpdate(
+      .findByIdAndUpdate(
         _id,
         {
           $inc: { [targetKey]: modifier },
