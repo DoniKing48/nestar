a18c03497c5f70d29ceae4d10421269ac353ce0d
DoniKing48 <overnelord@gmail.com>
2025-06-27 10:46:16 +0500

    feat: develop admin related boardArticle GraphQL APIs

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -11,7 +11,9 @@ export const availablePropertySorts = [
 	'propertyViews', 
 	'propertyRank', 
 	'propertyPrice'
-]; 
+];
+
+export const availableBoardArticleSorts = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews'];
 
 // IMAGE CONFIGURATION 
 import { v4 as uuidv4 } from 'uuid';
