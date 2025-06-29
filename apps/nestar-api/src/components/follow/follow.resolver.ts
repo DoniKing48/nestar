e7ee7beccfd97dc25811bf100ebaf159484311ee
DoniKing48 <overnelord@gmail.com>
2025-06-29 16:52:05 +0500

    feat: develop subscibe, unsubscibe, getMemberFollowings and getMemberFollowers GraphQL APIs

--- apps/nestar-api/src/components/follow/follow.resolver.ts
+++ apps/nestar-api/src/components/follow/follow.resolver.ts
@@ -1,7 +1,60 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { FollowService } from './follow.service';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { Follower, Followers, Followings } from '../../libs/dto/follow/follow';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import { ObjectId } from 'mongoose';
+import { shapeIntoMongoObjectId } from '../../libs/config';
+import { FollowInquiry } from '../../libs/dto/follow/follow.input';
 
 @Resolver()
 export class FollowResolver {
   constructor(private readonly followService: FollowService) {}
+
+  @UseGuards(AuthGuard)
+  @Mutation((returns) => Follower)
+  public async subscribe(
+    @Args('input') input: string,
+    @AuthMember('_id') memberId: ObjectId
+  ): Promise<Follower> {
+    console.log('Mutation: subscibe');
+    const followingId = shapeIntoMongoObjectId(input);
+    return await this.followService.subscibe(memberId, followingId);
+  }
+
+  @UseGuards(AuthGuard)
+  @Mutation((returns) => Follower)
+  public async unsubscribe(
+    @Args('input') input: string,
+    @AuthMember('_id') memberId: ObjectId
+  ): Promise<Follower> {
+    console.log('Mutation: unsubscibe');
+    const followingId = shapeIntoMongoObjectId(input);
+    return await this.followService.unsubscibe(memberId, followingId);
+  }  
+
+  @UseGuards(AuthGuard)
+  @Query((returns) => Followings)
+  public async getMemberFollowings(
+    @Args('input') input: FollowInquiry,
+    @AuthMember('_id') memberId: ObjectId
+  ): Promise<Followings> {
+    console.log('Query: getMemberFollowings');
+    const { followerId } = input.search;
+    input.search.followerId = shapeIntoMongoObjectId(followerId);
+    return await this.followService.getMemberFollowings(memberId, input);
+  }
+
+  @UseGuards(AuthGuard)
+  @Query((returns) => Followers)
+  public async getMemberFollowers(
+    @Args('input') input: FollowInquiry,
+    @AuthMember('_id') memberId: ObjectId
+  ): Promise<Followings> {
+    console.log('Query: getMemberFollowers');
+    const { followingId } = input.search;
+    input.search.followingId = shapeIntoMongoObjectId(followingId);
+    return await this.followService.getMemberFollowers(memberId, input);
+  }      
 }
--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -26,7 +26,7 @@ export const getSerialForImage = (filename: string) => {
 
 export const shapeIntoMongoObjectId = (target: any) => {
   return typeof target === 'string' ? new ObjectId(target) : target;
-}
+};
 
 export const lookupMember = {
 	$lookup: {
@@ -34,5 +34,23 @@ export const lookupMember = {
 		localField: 'memberId',
 		foreignField: '_id',
 		as: 'memberData',
+	},
+};
+
+export const lookupFollowingData = {
+	$lookup: {
+		from: 'members',
+		localField: 'followingId',
+		foreignField: '_id',
+		as: 'followingData',
+	},
+};
+
+export const lookupFollowerData = {
+	$lookup: {
+		from: 'members',
+		localField: 'followerId',
+		foreignField: '_id',
+		as: 'followerData',
 	},
 };
